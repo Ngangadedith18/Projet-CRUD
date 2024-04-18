3ea5012940a43898ae841fa00534cf63253e9b82
Ngangadedith18 <158487614+Ngangadedith18@users.noreply.github.com>
2024-04-18 18:29:01 +0100

    Add files via upload

---
+++ HospisManagement/src/main/java/controleur/Dossier_Actions.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controleur;
+
+import Dao.ConnexionBase;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modele.dossier;
+
+/**
+ *
+ * @author ideal
+ */
+public class Dossier_Actions {
+    //methode Dossier_Add
+    public void Dossier_Add(dossier d){
+        //connexion à la base
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        //requete sql
+        String rep="insert into dossier (Id, dateCreation, nature_Examen ,date_Examen ,Observations, codeP)values('"+d.getId()+"','"+d.getDateCreation()+"','"+d.getNature_Examen()+"','"+d.getDate_Examen()+"','"+d.getObservations()+"','"+d.getCodeP()+"')";
+    
+        try {
+            //execution
+            cb.st.executeUpdate(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Dossier_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    //methode Dossier_Update 
+    
+    public void Dossier_Update(dossier d){
+       //connexion
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        //requete
+        String rep="update dossier set Id='"+d.getId()+"',dateCreation='"+d.getDateCreation()+"',nature_Examen='"+d.getNature_Examen()+"',date_Examen='"+d.getDate_Examen()+"',Observations='"+d.getObservations()+"',codeP='"+d.getCodeP()+"'";
+        
+        try {
+            cb.st.executeUpdate(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Dossier_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+    //methode Dossier_Del
+     public void Dossier_Del(int Id){
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Delete from dossier where IdP='"+Id+"'";
+    
+        try {
+            cb.st.executeUpdate(rep);
+            //methode Dossier_Find
+            
+            //methode Dossier_Liste
+        } catch (SQLException ex) {
+            Logger.getLogger(Dossier_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+     }
+     
+    //methode Patient_Find
+    public dossier Dossier_Find(int Id){
+        dossier d=null;
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Select * from dossier where Id='"+Id+"'";
+        
+        ResultSet rs;
+        try { 
+            rs=cb.st.executeQuery(rep);
+           if (rs.next()){
+            d.setId(rs.getInt("Id"));
+            d.setDateCreation(rs.getString("dateCreation"));
+            d.setNature_Examen(rs.getString("nature_Examen"));
+            d.setDate_Examen(rs.getString("date_Examen"));
+            d.setObservations(rs.getString("Observations"));
+            d.setCodeP(rs.getInt("codeP"));
+            } 
+        } catch (SQLException ex) {
+            Logger.getLogger(Dossier_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      return d;  
+    }  
+
+    //methode Patient_Liste
+    public ResultSet Dossier_Liste(){
+        ResultSet rs=null;
+       ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Select * from dossier ";
+        
+        try {
+            rs=cb.st.executeQuery(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Dossier_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+}
+     
---
+++ HospisManagement/src/main/java/controleur/Patient_Actions.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controleur;
+
+import Dao.ConnexionBase;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import modele.patient;
+
+/**
+ *
+ * @author ideal
+ */
+public class Patient_Actions {
+    //methode Patient_Add
+    public void Patient_Add(patient p){
+        //connexion à la base
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        //requete sql
+        String rep="insert into patient (codeP, nom, prenom,age ,sexe ,adresse)values('"+p.getCodeP()+"','"+p.getNom()+"','"+p.getPrenom()+"','"+p.getAge()+"','"+p.getSexe()+"','"+p.getAdresse()+"')";
+    
+        try {
+            //execution
+            cb.st.executeUpdate(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+        
+    //methode Patient_Update
+    public void Patient_Update(patient p){
+       //connexion
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        //requete
+        String rep="update patient set codeP='"+p.getCodeP()+"',nom='"+p.getNom()+"',prenom='"+p.getPrenom()+"',age='"+p.getAge()+"',sexe='"+p.getSexe()+"',adresse='"+p.getAdresse()+"'";
+        
+        try {
+            //execution
+            cb.st.executeUpdate(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //methode Patient_Del
+    public void Patient_Del(int codeP){
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Delete from patient where codeP='"+codeP+"'";
+        try {
+            cb.st.executeUpdate(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+    //methode Patient_Find
+    public patient Patient_Find(int codeP){
+        patient p=null;
+        ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Select * from patient where codeP='"+codeP+"'";
+        
+        ResultSet rs;
+        try {
+            rs=cb.st.executeQuery(rep);
+            if (rs.next()){
+            p.setCodeP(rs.getInt("codeP"));
+            p.setNom(rs.getString("nom"));
+            p.setPrenom(rs.getString("prenom"));
+            p.setAge(rs.getString("age"));
+            p.setSexe(rs.getString("sexe"));
+            p.setAdresse(rs.getString("adresse"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return p;    
+    }
+    
+    
+    //methode Patient_Liste
+    public ResultSet Patient_Liste(){
+        ResultSet rs=null;
+       ConnexionBase cb=new ConnexionBase();
+        cb.connection();
+        
+        String rep="Select * from patient ";
+
+        try {
+            rs=cb.st.executeQuery(rep);
+        } catch (SQLException ex) {
+            Logger.getLogger(Patient_Actions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+        
+    }
+    
+}
